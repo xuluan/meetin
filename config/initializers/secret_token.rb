81257921ba1d6d3cf456338e4e5d843d571c14d3
xuluan <rolle.xu@gmail.com>
2012-07-21 05:43:59 -0700

    create meetin

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Meetin::Application.config.secret_token = 'd5763702f25b2d41d997f6e1c01d2b9a639a9b81a3bd639c1e469f8f40dba7ce79ab91dd4f6797b981823945009cef8ecad9c8969857f336bb3e9ff3291beea8'
