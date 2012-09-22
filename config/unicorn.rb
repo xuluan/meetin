16217f39a2cc8209f1a88373a926615e5cf40c5d
xuluan <rolle.xu@gmail.com>
2012-09-22 05:02:57 -0700

    add unicorn.rb

---
+++ config/unicorn.rb
@@ -0,0 +1,8 @@
+working_directory "/home/ubuntu/git/meetin/config"
+pid "/home/ubuntu/git/meetin/config/tmp/pids/unicorn.pid"
+stderr_path "/home/ubuntu/git/meetin/config/log/unicorn.log"
+stdout_path "/home/ubuntu/git/meetin/config/log/unicorn.log"
+
+listen "/tmp/unicorn.todo.sock"
+worker_processes 2
+timeout 30
