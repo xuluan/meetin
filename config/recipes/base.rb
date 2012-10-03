a8e55c968b99f7dad4b077d26dd0f524ff376bf3
xuluan <rolle.xu@gmail.com>
2012-10-03 07:23:30 -0700

    test deploy

--- config/recipes/base.rb
+++ config/recipes/base.rb
@@ -12,5 +12,14 @@ namespace :deploy do
   task :install do
     run "#{sudo} apt-get -y update"
     run "#{sudo} apt-get -y install python-software-properties"
+    run "#{sudo} apt-get -y install curl"
+    run "#{sudo} apt-get -y install build-essential zlib1g-dev git-core sqlite3 libsqlite3-dev libsqlite3-0"
+    run "#{sudo} apt-get -y install libxslt-dev libxml2-dev"
+    run "#{sudo} apt-get -y install libreadline-dev libreadline6 libreadline6-dev"
+    run "#{sudo} apt-get -y install libssl-dev openssl"
+    run "#{sudo} apt-get -y install libyaml-dev"
+    run "#{sudo} apt-get -y install autoconf libc6-dev ncurses-dev automake libtool bison"
+    run "#{sudo} apt-get -y install zlib1g zlib1g-dev"
+    run "#{sudo} apt-get -y install fcgiwrap"
   end
 end
