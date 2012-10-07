bf1add6c2e0147f1c1a8cd14ff97acac07a6bb0a
xuluan <rolle.xu@gmail.com>
2012-10-08 04:30:49 +0800

    rm some files for old deploy way

--- config/meetin.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-source /usr/local/rvm/scripts/rvm
-rvm use 1.9.3
-APP_ROOT=/home/ubuntu/git/meetin
-CMD="$APP_ROOT/bin/unicorn -D -c $APP_ROOT/config/unicorn.rb -E production"
-$CMD
--- config/unicorn.rb
+++
@@ -1,8 +0,0 @@
-working_directory "/home/ubuntu/git/meetin"
-pid "/home/ubuntu/git/meetin/tmp/pids/unicorn.pid"
-stderr_path "/home/ubuntu/git/meetin/log/unicorn.log"
-stdout_path "/home/ubuntu/git/meetin/log/unicorn.log"
-
-listen "/tmp/unicorn.meetin.sock"
-worker_processes 2
-timeout 30
