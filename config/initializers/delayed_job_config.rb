9495f1b8f111e0f26421d51a96efdbc0f4f6b8eb
xuluan <rolle.xu@gmail.com>
2012-10-16 07:34:21 -0700

    disable delayed job in development

--- config/initializers/delayed_job_config.rb
+++ config/initializers/delayed_job_config.rb
@@ -4,4 +4,4 @@ Delayed::Worker.sleep_delay = 60
 Delayed::Worker.max_attempts = 3
 Delayed::Worker.max_run_time = 5.minutes
 Delayed::Worker.read_ahead = 10
-Delayed::Worker.delay_jobs = !Rails.env.test?
+Delayed::Worker.delay_jobs = Rails.env.production?
