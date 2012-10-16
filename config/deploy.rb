c14b709981911790098806faef2a9ac4a3b9080a
xuluan <rolle.xu@gmail.com>
2012-10-16 05:37:19 -0700

    add delayed job config, deploy for delayed job

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,5 @@
 require "bundler/capistrano"
-
+require "delayed/recipes"  
 load "config/recipes/base"
 load "config/recipes/nginx"
 load "config/recipes/unicorn"
@@ -8,6 +8,7 @@ load "config/recipes/check"
 
 server "50.18.176.76", :web, :app, :db, primary: true
 
+set :rails_env, "production" #added for delayed job  
 set :user, "ubuntu"
 set :application, "meetin"
 set :deploy_to, "/home/#{user}/apps/#{application}"
@@ -20,5 +21,9 @@ set :branch, "release"
 default_run_options[:pty] = true
 ssh_options[:forward_agent] = true
 ssh_options[:keys] = ["/home/xuluan/.ssh/ec2_ca.pem"]
+
+after "deploy:stop",    "delayed_job:stop"
+after "deploy:start",   "delayed_job:start"
+after "deploy:restart", "delayed_job:restart"
 
 after "deploy", "deploy:cleanup" # keep only the last 5 releases
