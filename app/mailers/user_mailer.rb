ee2f5f16d34db437c08bf885f86b5d288dba51bf
xuluan <rolle.xu@gmail.com>
2012-11-12 05:32:17 -0800

    reply to organizer

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -8,8 +8,10 @@ class UserMailer < ActionMailer::Base
   #
   def meeting_invite(email, meeting)
     @meeting = meeting
+    @organizer = meeting.organizer.email
 
-    mail from: meeting.organizer.email, to: email, subject: "Meeting invitation: #{meeting.title}"do |format|
+    mail reply_to: @organizer, from: @organizer, to: email, 
+         subject: "Meeting invitation: #{meeting.title}" do |format|
       format.html
     end
   end
